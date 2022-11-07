7f3b47eab8f981e6368ee92926fffeb0a7fce5cf
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-11-07 23:46:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,5 @@ elif choice == 'Test':
 elif choice == 'Contact':
     st.title('Contact')
     st.subheader('[Abubakr Mamajonov](https://abubakr1710-my-website-app-kx4y2h.streamlit.app)')
-    img = 'images\qrcode.png'
-    st.image(img, width=400)
 
 
